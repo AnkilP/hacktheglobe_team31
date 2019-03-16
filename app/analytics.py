a8b2e61d6e49bd6c17bb7706c63b5e27789d6b9a
AnkilP <a293pate@uwaterloo.ca>
2019-03-16 18:18:03 -0400

    gcp integrate

--- app/analytics.py
+++ app/analytics.py
@@ -1,10 +1,15 @@
-import django
-import numpy as np
-import json
+import os
+import requests
+from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
 
 class my_class(object):
 	
 	def __init__(self):
+		pass
+
+	@app.route
+	def get_survey_data(self, survey_json):
 
 
 
