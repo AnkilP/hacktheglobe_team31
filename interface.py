948c0cdd281a0caf94cea0200bb10bf57e6787b5
Ankil Patel <a293pate@uwaterloo.ca>
2019-03-16 16:41:17 -0400

    initial commit with interfac object - rough draft

---
+++ interface.py
@@ -0,0 +1,28 @@
+class interface(object):
+
+	def __init__(self):
+		self.locations = []
+		self.services = []
+
+	def get_job_information(self, skills):
+		#something with skills		
+		#return array of locations
+		location = []
+		self.locations.append(location)
+
+	def get_mentorship_information(self, skills):
+		#return array of locations
+		location = []
+		self.locations.append(location)
+
+	def get_service_information(self, city):
+		#what kind of services are available in the city		
+		service = []
+		self.services.append(service)
+
+	def get_housing_information(self, region):
+		#return locations of optimal housing districts
+		region = []
+		return region
+
+
