fff91e30b41558ed3a6ec7f8f4fb0e49a1e846ea
AnkilP <a293pate@uwaterloo.ca>
2019-03-17 08:39:08 -0400

    restructured files

--- app/analytics.py
+++ app/analytics.py
@@ -1,17 +1,34 @@
-import os
-import requests
-from flask import Flask, render_template, request
-from flask.ext.sqlalchemy import SQLAlchemy
+from __future__ import absolute_import, division, print_function
 
-class my_class(object):
-	
-	def __init__(self):
-		pass
+import numpy as np
+import pandas as pd
 
-	@app.route
-	def get_survey_data(self, survey_json):
+import tensorflow as tf
 
+from tensorflow import feature_column
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
 
+dataframe = pd.read_csv("PopCPI.csv", engine = "python")
 
+train, test = train_test_split(dataframe, test_size=0.2)
+train, val = train_test_split(train, test_size=0.2)
 
+def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+  dataframe = dataframe.copy()
+  labels = dataframe.pop('target')
+  ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+  if shuffle:
+    ds = ds.shuffle(buffer_size=len(dataframe))
+  ds = ds.batch(batch_size)
+  return ds
 
+batch_size = 5 # A small batch sized is used for demonstration purposes
+train_ds = df_to_dataset(train, batch_size=batch_size)
+val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
+for feature_batch, label_batch in train_ds.take(1):
+  print('Every feature:', list(feature_batch.keys()))
+  print('A batch of ages:', feature_batch['age'])
+  print('A batch of targets:', label_batch )
