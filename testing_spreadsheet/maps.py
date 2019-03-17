e6ca438d99e3c589f870024bbead050ab5e1c723
AnkilP <a293pate@uwaterloo.ca>
2019-03-17 07:32:52 -0400

    security risk

--- testing_spreadsheet/maps.py
+++
@@ -1,7 +0,0 @@
-import googlemaps
-from datetime import datetime
-
-gmaps = googlemaps.Client(key='AIzaSyDP3PPszP_ZndSOKX4g4r7rf649c6Hea0U')
-
-# Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
